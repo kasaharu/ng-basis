c2ee3da854287e65ed134c8c592cca3d3b9628c8
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-30 09:59:53 +0900

    Remove stencil project

--- projects/stencil/src/lib/stencil.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { StencilComponent } from './stencil.component';
-
-@NgModule({
-  declarations: [StencilComponent],
-  imports: [],
-  exports: [StencilComponent],
-})
-export class StencilModule {}
--- projects/stencil/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of stencil
- */
-
-export * from './lib/stencil.service';
-export * from './lib/stencil.component';
-export * from './lib/stencil.module';
